b69dd8b570765a3045648534c87e6397cf4473f7
Ksenia <olgasvmg@yandex.ru>
2024-08-09 16:39:34 +0300

    Готовое дз

---
+++ homework/1.py
@@ -0,0 +1,22 @@
+example = "Привет"
+print(example[0])
+print(example[-1:])
+print(example[3:6])
+print(example[::-1])
+print(example[1::2])
+
+Kolvo_DZ = 12
+Kolvo_chasov = 1.5
+Proga = "Python"
+Kolvoob = Kolvo_chasov / Kolvo_DZ
+print(Kolvoob)
+print("Курс:",Proga,",","затрачено часов:",Kolvo_chasov,",","среднее время выполнения:",Kolvoob,".")
+
+
+
+
+
+
+
+
+
