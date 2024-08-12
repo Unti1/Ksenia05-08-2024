3bf0105b8b96a455bd7977bb898cf94f449472a8
= <=>
2024-08-12 12:09:44 +0300

    Новая тема

---
+++ lessons/test.py
@@ -0,0 +1,7 @@
+name = input("Введите ваше имя: ")
+current_year = 2024
+birth_year = int(input("Введите год вашего рождения: "))
+user_age = current_year - birth_year
+
+
+print(f'Здравствуйте, {name}. В этом году вам:  {user_age}')
