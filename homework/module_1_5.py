e392721c5c0abd68c2329c508303abc627247cd5
Ksenia <olgasvmg@yandex.ru>
2024-08-17 12:19:21 +0300

    Готовое ДЗ

---
+++ homework/module_1_5.py
@@ -0,0 +1,20 @@
+immutable_var = (15, 10, 16)
+immutable_var1 = ("Привет", "мир", "25")
+immutable_var3 = (10, "Привет", 25, 29)
+print(immutable_var1) 
+print(immutable_var) 
+print(immutable_var3) 
+
+''' tuple - это кортеж и его изменить нельзя. 
+print(immutable_var)
+immutable_var.append(4)
+print(immutable_var) '''
+
+
+mutable_list = [15, 10, 16]
+mutable_list1 = ["Привет", "мир", "25"]
+mutable_list2= [10, "Привет", 25, 29]
+print(mutable_list) 
+print(mutable_list1) 
+print(mutable_list2) 
+
--- lessons/test.py
+++
@@ -1,7 +0,0 @@
-name = input("Введите ваше имя: ")
-current_year = 2024
-birth_year = int(input("Введите год вашего рождения: "))
-user_age = current_year - birth_year
-
-
-print(f'Здравствуйте, {name}. В этом году вам:  {user_age}')
