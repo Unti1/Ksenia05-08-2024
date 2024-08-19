ba98b5e1a440194011b465c0c12dea114a9230b8
= <=>
2024-08-19 12:08:33 +0300

    Новая тема:    - Словари

--- homework/module_1_5.py
+++ homework/module_1_5.py
@@ -5,15 +5,16 @@ print(immutable_var1)
 print(immutable_var) 
 print(immutable_var3) 
 
-''' tuple - это кортеж и его изменить нельзя. 
-print(immutable_var)
-immutable_var.append(4)
-print(immutable_var) '''
+# tuple - это кортеж и его изменить нельзя. 
+# print(immutable_var)
+# immutable_var.append(4)
+# print(immutable_var)
 
 
 mutable_list = [15, 10, 16]
 mutable_list1 = ["Привет", "мир", "25"]
 mutable_list2= [10, "Привет", 25, 29]
+mutable_list1.append(4)
 print(mutable_list) 
 print(mutable_list1) 
 print(mutable_list2) 
