f580f96758f87e296064f3091ee28466ae8d792d
Ksenia <olgasvmg@yandex.ru>
2024-10-27 20:52:04 +0300

    Готовое ДЗ

---
+++ homework/fake_math.py
@@ -0,0 +1,9 @@
+def divide(first,second):
+    if second ==0:
+        return('Ошибка')
+    else:
+        return first//second
+if __name__ == '__main__':    
+    print(divide(0,0))
+
+
---
+++ homework/module_4_1.py
@@ -0,0 +1,11 @@
+import fake_math as FM
+import true_math as TM
+
+result1=FM.divide(69, 3)
+result2 = FM.divide(3, 0)
+result3 = TM.divide(49, 7)
+result4 = TM.divide(15, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
