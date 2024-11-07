d21e524cfa21f7a4be6fc7443da40e7d00048946
Until <kostyuchenko.work@gmail.com>
2024-11-07 11:23:16 +0300

    Changes to be committed: 	modified:   homework/module_5_3.py 	modified:   lessons/module_2/func.ipynb 	modified:   lessons/module_5/classes.ipynb

--- homework/module_5_3.py
+++ homework/module_5_3.py
@@ -85,38 +85,35 @@ class House:
     def __eq__(self, other):
         if isinstance(other, House):
             return self.number_of_floors == other.number_of_floors
-        return NotImplemented
-
+    
     def __lt__(self,other):
         if isinstance(other, House):
             return other.number_of_floors > self.number_of_floors
-        return NotImplemented
-    
+        
     def __le__(self,other):
         if isinstance(other, House):
             return other.number_of_floors <= self.number_of_floors
-        return NotImplemented
-    
+        
     def __gt__(self,other):
         if isinstance(other, House):
             return other.number_of_floors > self.number_of_floors
-        return NotImplemented
-    
+        
     def __ge__(self,other):
         if isinstance(other, House):
             return other.number_of_floors >= self.number_of_floors
-        return NotImplemented
-    
+        
     def __ne__(self,other):
         if isinstance(other, House):
-            return other.number_of_floors != other.number_of_floors
-        return NotImplemented
+            return other.number_of_floors != other.number_of_floors    
     
     def __add__(self, value):
         if isinstance(value, int):
             self.number_of_floors += value
             return self
-        return NotImplemented
+        elif isinstance(value, House):
+            self.number_of_floors += value.number_of_floors
+            return self
+        
      
     def __radd__(self, value):
         return self.__add__(value)
